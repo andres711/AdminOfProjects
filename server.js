2bf15d3d135a9deed4dd1aa9fc4340c339068127
F. Andrés Villegas <avillegas332@gmail.com>
2023-02-07 18:04:05 -0300

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer("/welcome",(req, res) => {
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
