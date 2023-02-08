ff8f8cd9cabd1bd7647c41cf907c13cb35912fc0
F. Andrés Villegas <avillegas332@gmail.com>
2023-02-08 16:46:21 -0300

    change api domain

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clientAxios = axios.create({
-    baseURL: process.env.REACT_APP_API || "http://localhost:3001",
+    baseURL: "adminofprojects-production.up.railway.app",
 });
 
 export default clientAxios;
