e40e6c4ed65aa707fff0412c174da7c4f2a6ec45
F. Andrés Villegas <avillegas332@gmail.com>
2023-02-08 21:48:57 -0300

    fix

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const clientAxios = axios.create({
-    baseURL: process.env.REACT_APP_API || "http://localhost:3001",
+    // baseURL: process.env.REACT_APP_API || "http://localhost:3001",
+    baseURL: "https://adminofprojects-production.up.railway.app"
 });
 
 export default clientAxios;
