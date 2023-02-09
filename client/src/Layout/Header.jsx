ed87462761bd99725c0efe9e5445c24b1518ef3d
F. Andrés Villegas <avillegas332@gmail.com>
2023-02-09 17:55:58 -0300

    add sweet alert

--- client/src/Layout/Header.jsx
+++ client/src/Layout/Header.jsx
@@ -1,10 +1,27 @@
-import { useContext,useEffect } from "react";
-
+import { useContext, useNavigate} from "react";
 import AuthContext from "../Context/Auth/context.js";
 
+
+import swal from 'sweetalert';
+
+
 function Header() {
-  const authContext = useContext(AuthContext);
-  const { user } = authContext;
+  const authState = useContext(AuthContext);
+  const { user,closeSesion } = authState;
+  const navigate = useNavigate();
+  
+  
+  const handlerButtonCloseSession = ()=>{
+    swal({
+      title: "Are you sure?",
+      text: "Are you sure close session?",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    })
+    navigate("/closeSesion");
+
+  }
   
   return (
     <header className="app-header">
@@ -19,7 +36,7 @@ function Header() {
       )}
 
       <nav className="nav-principal">
-        <a href="http://localhost:3000/closeSesion">Close Session</a>
+        <button className="btn btn-blank" onClick={()=>handlerButtonCloseSession()}>Close Session</button>
       </nav>
     </header>
   );
